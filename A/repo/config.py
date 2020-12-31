3d6dec2913e9437bf0ed5b95ee55e1b6e73ecbcb
Vitoria Leite <vitoria.leite.ismart@gmail.com>
2020-12-31 00:46:48 -0300

    first commit

---
+++ A/repo/config.py
@@ -0,0 +1,10 @@
+import os
+
+path_projeto = r'D:\Users\vicml\Documents\Projetos\CompeticaoAtuaria\A'
+
+path_dados = os.path.join(path_projeto,'dados')
+
+path_dados_entrada = os.path.join(path_dados,'entrada')
+path_dados_tratados = os.path.join(path_dados,'tratados')
+
+path_arquivo_treino = os.path.join(path_dados_entrada,'training.csv')
---
+++ B/repo/config.py
@@ -0,0 +1,11 @@
+import os
+
+path_projeto = r'D:\Users\vicml\Documents\Projetos\CompeticaoAtuaria\B'
+
+path_dados = os.path.join(path_projeto,'dados')
+
+path_dados_entrada = os.path.join(path_dados,'entrada')
+path_dados_tratados = os.path.join(path_dados,'tratados')
+
+path_arquivo_treino = os.path.join(path_dados_entrada,'train.csv')
+path_arquivo_test = os.path.join(path_dados_entrada,'test.csv')
